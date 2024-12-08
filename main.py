5e6948e3d4ccaffbd367b7318e4b60d18ce9ecec
Angie <angel890817@hotmail.com>
2024-12-08 15:59:18 +1100

    check point 1

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from file_operation import load_matches, save_matches
+from match_operation import display_matches
 
 spam = load_matches('matches.json')
-
-spam[0]['Score A'] = 3
-
-save_matches('debug.json', spam)
+display_matches(spam)
